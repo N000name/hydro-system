b6b8cdf203aade15a7ef6c724b869dc147717a43
N000name <2195546325@qq.com>
2026-07-13 14:20:04 +0800

    xiugai

---
+++ WaterLevel.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class WaterLevel{
+ public static void main(String[]args){
+   Scanner sc=new Scanner(System.in);
+   double max=0,min=999,sum=0;
+   int count=0;
+    System.out.println("Input water level,enter -1 to stop");
+  while(true){
+     double num=sc.nextDouble();
+    if(num==-1)break;
+     sum+=num;
+    if(num>max)max=num;
+    if(num<min)min=num;
+    count++;
+}
+     double avg=sum/count;
+     System.out.printf("Average: %.2f m\n ",avg);
+     System.out.printf("Max: %.2f m\n ",max);
+
+     System.out.printf("Min: %.2f m\n ",min);
+      if(max>25.0){
+  System.out.println("Warning:Water level over 25m");
+} else{
+System.out.println ("Water level normal");
+}
+sc.close();
+}}
